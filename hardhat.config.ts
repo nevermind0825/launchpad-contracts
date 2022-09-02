cc98d3dd459b45149d99b73bc8912fa38949078e
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-02 13:38:02 +0900

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-solhint";
+import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
+import "hardhat-contract-sizer";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomiclabs/hardhat-etherscan";
-import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
