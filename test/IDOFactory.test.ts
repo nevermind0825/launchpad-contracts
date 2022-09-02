56a076577595660bed72829d0e417cf7c1a493f2
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-02 19:40:40 +0900

    Update test for the IDOFactory contract

--- test/IDOFactory.test.ts
+++ test/IDOFactory.test.ts
@@ -5,8 +5,6 @@ import { expect } from "chai";
 
 describe("IDOFactory test", () => {
   let IDOFactory: Contract;
-  let Tier: Contract;
-  let Point: Contract;
   let Play: Contract;
   let PlayBUSD: Contract;
   let owner: SignerWithAddress;
@@ -21,12 +19,12 @@ describe("IDOFactory test", () => {
 
     // Deploy tier contract.
     const tier = await ethers.getContractFactory("Tier");
-    Tier = await tier.deploy();
+    const Tier = await tier.deploy();
     await Tier.deployed();
 
     // Deploy point contract.
     const point = await ethers.getContractFactory("Point");
-    Point = await point.deploy(1); // set decimal as 1.
+    const Point = await point.deploy(1); // set decimal as 1.
     await Point.deployed();
 
     // Play contract deploy.
@@ -47,74 +45,130 @@ describe("IDOFactory test", () => {
     // Users fund into the Play token and PlayBUSD token.
     await Play.transfer(user0.address, 100);
     await Play.transfer(user1.address, 200);
-
     await PlayBUSD.transfer(user0.address, 500);
     await PlayBUSD.transfer(user1.address, 300);
 
-    // Add tokens
+    // Insert tokens
     await Point.insertToken(Play.address, 8);
     await Point.insertToken(PlayBUSD.address, 15);
-  });
 
-  it("Insert and remove opertors", async () => {
-    await expect(IDOFactory.connect(user0).insertOperator(operator.address)).to.be.revertedWith(
-      "Ownable: caller is not the owner",
-    );
-    await expect(IDOFactory.connect(user0).removeOperator(0)).to.be.revertedWith("Ownable: caller is not the owner");
-    await expect(IDOFactory.removeOperator(0)).to.be.revertedWith("IDOFactory: operator index is invalid");
+    // Insert operator
     await IDOFactory.connect(owner).insertOperator(operator.address);
-    await expect(IDOFactory.insertOperator(operator.address)).to.be.revertedWith(
-      "IDOFactory: you have already inserted the operator",
-    );
-    await IDOFactory.connect(owner).insertOperator(user1.address);
-    await IDOFactory.removeOperator(0);
   });
 
-  it("Create and get IDO and set fee percent and fee recipient", async () => {
-    await IDOFactory.insertOperator(operator.address);
+  describe("Check owner functions", async () => {
+    it("Check inertOperator function", async () => {
+      await expect(IDOFactory.connect(user0).insertOperator(operator.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
 
-    await expect(IDOFactory.connect(user0).createIDO(Play.address, 1000, PlayBUSD.address, 5000)).to.be.revertedWith(
-      "IDOFactory: caller is not the operator",
-    );
-    await expect(IDOFactory.connect(operator).createIDO(Play.address, 0, PlayBUSD.address, 0)).to.be.revertedWith(
-      "IDO: amount must be greater than zero",
-    );
-    await IDOFactory.connect(operator).createIDO(Play.address, 1000, PlayBUSD.address, 5000);
+    it("Check removeOperator function", async () => {
+      await expect(IDOFactory.connect(user0).removeOperator(0)).to.be.revertedWith("Ownable: caller is not the owner");
+    });
 
-    await expect(IDOFactory.getIDO(1)).to.be.revertedWith("IDOFactory: IDO index is invalid");
-    console.log("\tIDO address:", await IDOFactory.getIDO(0));
+    it("Check setFeePercent function", async () => {
+      await expect(IDOFactory.connect(user0).setFeePercent(10)).to.be.revertedWith("Ownable: caller is not the owner");
+    });
 
-    await expect(IDOFactory.setFeeRecipient(ethers.constants.AddressZero)).to.be.revertedWith(
-      "IDOFactory: fee recipient must not be address(0)",
-    );
-    await expect(IDOFactory.connect(user0).setFeeRecipient(recipient.address)).to.be.revertedWith(
-      "Ownable: caller is not the owner",
-    );
-    await IDOFactory.setFeeRecipient(recipient.address);
+    it("Check setFeeReciepent function", async () => {
+      await expect(IDOFactory.connect(user0).setFeeRecipient(recipient.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
 
-    await expect(IDOFactory.setFeePercent(0)).to.be.revertedWith("IDOFactory: fee percent must be bigger than zero");
-    await expect(IDOFactory.connect(user0).setFeePercent(10)).to.be.revertedWith("Ownable: caller is not the owner");
-    await IDOFactory.setFeePercent(10);
+    it("Check finalizeIDO function", async () => {
+      await expect(IDOFactory.connect(user1).finalizeIDO(0, finalizer.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
+
+    it("Check emergencyRefund function", async () => {
+      await expect(IDOFactory.connect(user1).emergencyRefund(0)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
   });
 
-  it("Finalize IDO", async () => {
-    await IDOFactory.insertOperator(operator.address);
-    await PlayBUSD.transfer(operator.address, 5000);
-    await expect(IDOFactory.connect(operator).createIDO(Play.address, 1000, PlayBUSD.address, 5000)).to.be.revertedWith(
-      "IDOFactroy: balance of owner is not enough",
-    );
-    await IDOFactory.connect(operator).createIDO(Play.address, 1000, PlayBUSD.address, 4200);
-    await expect(IDOFactory.finalizeIDO(0, finalizer.address)).to.be.revertedWith(
-      "IDOFactory: owner didn't set the fee percent",
-    );
-    await IDOFactory.setFeePercent(10);
-    await expect(IDOFactory.finalizeIDO(0, finalizer.address)).to.be.revertedWith(
-      "IDOFactory: owner didn't set the fee recipient",
-    );
-    await IDOFactory.setFeeRecipient(recipient.address);
-    await expect(IDOFactory.connect(user1).finalizeIDO(0, finalizer.address)).to.be.revertedWith(
-      "Ownable: caller is not the owner",
-    );
-    await IDOFactory.finalizeIDO(0, finalizer.address);
+  describe("Check operator functions", async () => {
+    it("Check removeOperator function", async () => {
+      await expect(IDOFactory.removeOperator(1)).to.be.revertedWith("IDOFactory: operator index is invalid");
+      await IDOFactory.connect(owner).insertOperator(user1.address);
+      await IDOFactory.removeOperator(0);
+    });
+
+    it("Check insertOperator function", async () => {
+      await expect(IDOFactory.insertOperator(operator.address)).to.be.revertedWith(
+        "IDOFactory: you have already inserted the operator",
+      );
+    });
+  });
+
+  describe("Check createIDO function", async () => {
+    it("Check operator", async () => {
+      await expect(IDOFactory.connect(user0).createIDO(Play.address, 1000, PlayBUSD.address, 5000)).to.be.revertedWith(
+        "IDOFactory: caller is not the operator",
+      );
+    });
+
+    it("Check parameters of createIDO function", async () => {
+      await expect(IDOFactory.connect(operator).createIDO(Play.address, 0, PlayBUSD.address, 0)).to.be.revertedWith(
+        "IDO: amount must be greater than zero",
+      );
+    });
+
+    it("Check balance of owenr", async () => {
+      await expect(
+        IDOFactory.connect(operator).createIDO(Play.address, 1000, PlayBUSD.address, 50000),
+      ).to.be.revertedWith("IDOFactroy: balance of owner is not enough");
+    });
+
+    it("Check getIDO funtion", async () => {
+      await expect(IDOFactory.getIDO(1)).to.be.revertedWith("IDOFactory: IDO index is invalid");
+    });
+  });
+
+  describe("Set fee info functions", async () => {
+    it("Check setFeeRecipient function", async () => {
+      await expect(IDOFactory.setFeeRecipient(ethers.constants.AddressZero)).to.be.revertedWith(
+        "IDOFactory: fee recipient must not be address(0)",
+      );
+    });
+
+    it("Check setFeePercent function", async () => {
+      await expect(IDOFactory.setFeePercent(0)).to.be.revertedWith("IDOFactory: fee percent must be bigger than zero");
+    });
+  });
+
+  describe("Finalize IDO", async () => {
+    beforeEach(async () => {
+      await IDOFactory.connect(operator).createIDO(Play.address, 1000, PlayBUSD.address, 4200);
+    });
+
+    it("Check setting fee info before finalize", async () => {
+      await expect(IDOFactory.finalizeIDO(0, finalizer.address)).to.be.revertedWith(
+        "IDOFactory: owner didn't set the fee percent",
+      );
+      await IDOFactory.setFeePercent(10);
+      await expect(IDOFactory.finalizeIDO(0, finalizer.address)).to.be.revertedWith(
+        "IDOFactory: owner didn't set the fee recipient",
+      );
+    });
+
+    it("Check finalizeIDO function", async () => {
+      console.log("\tIDO address:", await IDOFactory.getIDO(0));
+      await IDOFactory.setFeePercent(10);
+      await IDOFactory.setFeeRecipient(recipient.address);
+      await IDOFactory.finalizeIDO(0, finalizer.address);
+    });
+
+    it("Emergency refund", async () => {
+      await IDOFactory.emergencyRefund(0);
+    });
+  });
+
+  it("Get multiplier of funder", async () => {
+     // user point = 830 (= 100 * 0.8 + 500 * 1.5), Star: 500, 5
+    expect(await IDOFactory.getMultiplier(user0.address)).to.equal(5);
   });
 });
